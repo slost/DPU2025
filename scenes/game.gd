2d3d0c2d370aba3801942558ba07e017ca318977
slost <slowslost@gmail.com>
2025-01-24 22:10:50 +0700

    Update Refs autoload

--- scenes/game.gd
+++ scenes/game.gd
@@ -15,32 +15,32 @@ var is_pause: bool = false
 
 func _ready() -> void:
 
-    draw_map()
+	draw_map()
 
 func _process(delta: float) -> void:
-    if is_pause:
-        return
+	if is_pause:
+		return
 
-    tiks += 1
+	tiks += 1
 
-    if tiks >= tik_per_move():
-        ents.move_snake()
-        current_moves += 1
-        print("Moves:" + str(current_moves))
-        tiks = 0
-        
+	if tiks >= tik_per_move():
+		ents.move_snake()
+		current_moves += 1
+		print("Moves:" + str(current_moves))
+		tiks = 0
+		
 
 
 func draw_map() -> void:
-    for x in range(map.get_size().x):
-        for y in range(map.get_size().y):
-            var coords = Vector2i(x, y)
-            if (x + y) % 2 == 0:
-                map.set_cell(coords, 0, Vector2(0, 0))  # Tile at (0, 0)
-            else:
-                map.set_cell(coords, 0, Vector2(1, 0))  # Tile at (0, 1)
-    ents.set_starting_position(Refs.Position.Middle)
+	for x in range(map.get_size().x):
+		for y in range(map.get_size().y):
+			var coords = Vector2i(x, y)
+			if (x + y) % 2 == 0:
+				map.set_cell(coords, 0, Vector2(0, 0))  # Tile at (0, 0)
+			else:
+				map.set_cell(coords, 0, Vector2(1, 0))  # Tile at (0, 1)
+	ents.set_starting_position(Refs.Position.Middle)
 
 func tik_per_move() -> int:
-    var fps:int = 60
-    return int(fps / current_speed)
+	var fps:int = 60
+	return int(fps / current_speed)
