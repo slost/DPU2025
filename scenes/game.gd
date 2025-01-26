6bdfeea3e77658ea141cf62cd4905d8d4244ccb2
Sithu <sithuandsithulove@gmail.com>
2025-01-26 11:55:18 +0700

    update_1

--- scenes/game.gd
+++ scenes/game.gd
@@ -10,6 +10,14 @@ extends Node2D
 
 @export var player_container :Node2D
 @export var _time :Timer
+@export var player_list_score :RichTextLabel
+@export var player_info_text :RichTextLabel
+@export var end_game_node :Control
+@export var player_list_end_game :RichTextLabel
+
+
+var start_game :bool
+var end_game :bool
 #//////////////////////////////
 
 
@@ -19,6 +27,9 @@ var is_pause: bool = false
 
 #players info
 var players_info :Dictionary
+
+
+var slip_point :float = 0.3
 
 #time
 var time_level :int
@@ -34,14 +45,15 @@ func _ready() -> void:
 	NetworkManager.client_connected.connect(_client_connected)
 	NetworkManager.client_disconnected.connect(_client_disconnected)
 	NetworkManager.server_disconnected.connect(_server_disconnected)
-
+	
+	end_game_node.visible = false
 	
 	if multiplayer.is_server():
 		for player_id :int in NetworkManager.players.keys():
 			players_info[player_id] = {
 				"Name" : NetworkManager.players[player_id]["Name"], 
 				"Direction" : "", "Position" : Vector2(), 
-				"Score" : 0, "Hitting" : false, 
+				"Score" : 0, "Hitting" : false, "Limit" : 0.0, 
 			}
 			
 			#sync plaeyr info
@@ -50,14 +62,14 @@ func _ready() -> void:
 			await get_tree().create_timer(0.1).timeout
 			_spawn_player.rpc(player_id)
 			_duplicate_anim.rpc(player_id)
-			
+			_update_player_list_score.rpc()
 
-			
+		
 		can_input = true
 		_sync_can_input.rpc(can_input)
 		#start time
 		_time.start()
-
+		$Limit_slppery.start()
 
 
 #Input
@@ -81,31 +93,37 @@ func _input(event: InputEvent) -> void:
 #pro
 func _physics_process(delta: float) -> void:
 	time_text.text = str(time_level)
+	_update_own_info()
 
 #Player inputing
 func _on_player_input_left(peer_id :int, direction :String) -> void:
 	print(peer_id, direction)
 	_player_movement(peer_id, direction)
+	_update_player_list_score.rpc()
 
 
 func _on_player_input_right(peer_id :int, direction :String) -> void:
 	print(peer_id, direction)
 	_player_movement(peer_id, direction)
+	_update_player_list_score.rpc()
 
 
 func _on_player_input_up(peer_id :int, direction :String) -> void:
 	print(peer_id, direction)
 	_player_movement(peer_id, direction)
+	_update_player_list_score.rpc()
 
 
 func _on_player_input_down(peer_id :int, direction :String) -> void:
 	print(peer_id, direction)
 	_player_movement(peer_id, direction)
+	_update_player_list_score.rpc()
 
 
 func _on_player_input_stop(peer_id :int, direction :String) -> void:
 	print(peer_id, direction)
 	_player_movement(peer_id, direction)
+	_update_player_list_score.rpc()
 
 
 #player movement
@@ -149,6 +167,25 @@ func _move_player(player_id: int, direction: String, distanc: int) -> void:
 		print("Error: Invalid direction '", direction, "'.")
 
 
+@rpc("authority", "reliable", "call_local")
+func _update_player_list_score() -> void:
+	player_list_score.text = ""
+	for player_name :int in players_info.keys():
+		player_list_score.text += "%s : score :%d |Slippery :%.3f\n" % [players_info[player_name]["Name"], players_info[player_name]["Score"], players_info[player_name]["Limit"]]
+
+func _update_own_info() -> void:
+	player_info_text.text = ""
+	if !players_info.is_empty() :player_info_text.text = "Name :%s | Score :%d" % [players_info[multiplayer.get_unique_id()]["Name"], players_info[multiplayer.get_unique_id()]["Score"]]
+
+@rpc("authority", "reliable", "call_local")
+func _check_limit_soap() -> void:
+	for player_id :int in players_info.keys():
+		if players_info[player_id]["Limit"] >= 5:
+			print(player_id, "END GAME")
+			end_game = true
+			_sync_end_game.rpc(end_game)
+			_end_game.rpc()
+
 #Animation direction
 @rpc("authority", "reliable", "call_local")
 func _anim_direction(peer_id :int, direction :String) -> void:
@@ -161,6 +198,8 @@ func _spawn_player(peer_id :int) -> void:
 	var player :Sprite2D = player_instance.instantiate() as Sprite2D
 	var plaeyr_face :int = NetworkManager.players[peer_id]["Face"]
 	player.name = str(peer_id)
+	player.position = Vector2(80, 584-140)
+	player.scale = Vector2(5, 5)
 	player._cosmetic(NetworkManager.players[peer_id]["Head"], NetworkManager.players[peer_id]["Face"], NetworkManager.players[peer_id]["Color"])
 	player_container.add_child(player, true)
 
@@ -214,13 +253,18 @@ func _sync_time_level(new_time :int) -> void:
 func _sync_can_input(can :bool) -> void:
 	can_input = can
 
+#sync_end_game
+@rpc("authority", "reliable")
+func _sync_end_game(new_end_game :bool) -> void:
+	end_game = new_end_game
+
 
 
 func _on_timer_timeout() -> void:
 	if !multiplayer.is_server(): return
 	time_level += 1
 	
-	if time_level >= 3:
+	if time_level >= 2:
 		for player_id :int in players_info.keys():
 			_move_player.rpc(player_id, players_info[player_id]["Direction"], 64)
 		for player_id :int in players_info.keys():
@@ -229,10 +273,57 @@ func _on_timer_timeout() -> void:
 			_sync_player_info.rpc(players_info)
 		time_level = 0
 	_sync_time_level.rpc(time_level)
+
+@rpc("authority", "reliable", "call_local")
+func _kill_player(peer_id :int) -> void:
+	_clean_node.rpc(peer_id)
+	_sync_can_input.rpc_id(peer_id, false)
+	
+
+
+func _on_b_soap_pressed() -> void:
+	if multiplayer.is_server():
+		players_info[multiplayer.get_unique_id()]["Score"] += 1
+		players_info[multiplayer.get_unique_id()]["Limit"] += slip_point
+		_sync_player_info.rpc(players_info)
+		_update_player_list_score.rpc()
+		_check_limit_soap.rpc()
+	else:
+		_request_add_score.rpc_id(1)
+
+
+@rpc("any_peer", "reliable")
+func _request_add_score() -> void:
+	if !multiplayer.is_server(): return
+	players_info[multiplayer.get_remote_sender_id()]["Score"] += 1
+	players_info[multiplayer.get_remote_sender_id()]["Limit"] += slip_point
+	_sync_player_info.rpc(players_info)
+	_update_player_list_score.rpc()
+	_check_limit_soap.rpc()
 	
 
 @rpc("authority", "reliable", "call_local")
-func _take_damage(peer_id :int) -> void:
-	var player_hitting :Array
-	player_hitting.append(peer_id)
-	print(player_hitting)
+func _end_game() -> void:
+	if end_game:
+		can_input = false
+		_sync_can_input.rpc(can_input)
+		end_game_node.visible = true
+		var max_score :int = 0
+		var winner :String
+		for player_id :int in players_info.keys():
+			if players_info[player_id]["Score"] > max_score:
+				max_score = players_info[player_id]["Score"]
+				winner = players_info[player_id]["Name"]
+			
+		player_list_end_game.text = ""
+		player_list_end_game.text = "WINNER IS %s!! SCORE :%d" % [winner, max_score]
+
+
+func _on_limit_slppery_timeout() -> void:
+	if !multiplayer.is_server(): return
+	for player_id :int in players_info.keys():
+		players_info[player_id]["Limit"] -= 0.1
+		if players_info[player_id]["Limit"] < 0.0:
+			players_info[player_id]["Limit"] == 0.0
+		_sync_player_info.rpc(players_info)
+	_update_player_list_score.rpc()
